e3479cd538fc958cc4b16730e1c498c49f93d5fb
Tigrino22 <valentin.legris@outlook.com>
2024-06-29 17:09:28 +0200

    PSR12

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file contains the CRUD for Restaurant's menu.
+ * 
+ * @category Controller_CRUD
+ * @package  Nothing
+ * @author   Valentin Legris <valentin.legris@outlook.com>
+ * @license  MIT www.abba.com
+ * @link     Nothing
+ */
 namespace App\Controller;
 
 use App\Entity\Menu;
@@ -14,15 +23,37 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route("api/menu", name: "api_app_menu_")]
-class MenuController extends AbstractController {
-
+/**
+ * MenuController
+ * CRUD for Restaurant's menu.
+ * 
+ * @category Class_file
+ * @package  Nothing
+ * @author   Valentin Legris <valentin.legris@outlook.com>
+ * @license  MIT www.abba.com
+ * @link     Nothing
+ */
+class MenuController extends AbstractController
+{
+    
+    /**
+     * Construct
+     *
+     * @param mixed $manager
+     * @param mixed $restaurantRepository
+     */
     public function __construct(private EntityManagerInterface $manager, private RestaurantRepository $restaurantRepository, 
-                    private MenuRepository $menuRepository)
-    {
+        private MenuRepository $menuRepository
+    ) {
         
     }
 
-    #[Route("/", name: "create", methods: ["POST"])]
+    #[Route("/", name: "create", methods: ["POST"])]    
+    /**
+     * Create
+     *
+     * @return Response
+     */
     public function create(): Response
     {
         $restaurant = $this->restaurantRepository->findOneBy(["id" => 2]);
@@ -37,34 +68,58 @@ class MenuController extends AbstractController {
         $this->manager->persist($menu);
         $this->manager->flush();
 
-        return $this->json([
+        return $this->json(
+            [
             "message" => "Menu was created with uuid : {$menu->getUuid()}"
-        ]);
+            ]
+        );
     }
 
-    #[Route("/{id}", name: "show", methods: ["GET"], requirements: ["id" => "\d+"])]
+    #[Route("/{id}", name: "show", methods: ["GET"], requirements: ["id" => "\d+"])]    
+    /**
+     * Show
+     *
+     * @param  mixed $id
+     * @return Response
+     */
     public function show(int $id): Response
     {
         $menu = $this->menuRepository->findOneBy(["id" => $id]);
 
-        return $this->json([
+        return $this->json(
+            [
             "message" => "Menu was found with uuid : {$menu->getUuid()}"
-        ]);
+            ]
+        );
     }
 
-    #[Route("/{id}", name: "edit", methods: ["PUT"], requirements: ["id" => "\d+"])]
+    #[Route("/{id}", name: "edit", methods: ["PUT"], requirements: ["id" => "\d+"])]    
+    /**
+     * Edit
+     *
+     * @param  mixed $id
+     * @return Response
+     */
     public function edit(int $id): Response
     {
         $menu = $this->menuRepository->findOneBy(["id" => $id]);
         $menu->setTitle("Title menu modified");
         $menu->setUpdatedAt(new DateTime());
 
-        return $this->json([
+        return $this->json(
+            [
             "message" => "Menu was modifier with uuid : {$menu->getUuid()}, new name : {$menu->getTitle()} at {$menu->getUpdatedAt()->format('Y-m-d H:i:s')}"
-        ]);
+            ]
+        );
     }
 
-    #[Route("/{id}", name: "delete", methods: ["DELETE"], requirements: ["id" => "\d+"])]
+    #[Route("/{id}", name: "delete", methods: ["DELETE"], requirements: ["id" => "\d+"])]    
+    /**
+     * Delete
+     *
+     * @param  mixed $id
+     * @return Response
+     */
     public function delete(int $id): Response
     {
         $menu = $this->menuRepository->findOneBy(["id" => $id]);
@@ -72,8 +127,10 @@ class MenuController extends AbstractController {
         $this->manager->remove($menu);
         $this->manager->flush();
 
-        return $this->json([
+        return $this->json(
+            [
             "message" => "Menu with uuid {$menu->getUuid()} was deleted."
-        ]);
+            ]
+        );
     }
 }
